f8a8c2352db3d3cafdcacf83f077a3149bcb369d
gihanchaturanga <gihanchaturanga98@gmail.com>
2023-04-25 14:19:09 +0530

    SimpleInfoCard.jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { Grid } from '@mui/material';
 import './App.css';
 import Sidebar from './Elements/SIdebar/ResponsiveDrawer'
-import NewSelect from './Elements/Select/NewSelect';
+import SimpleInfoCard from './Elements/SimpleInfoCard/SimpleInfoCard';
 
 function App() {
   return (
     <Sidebar>
-      <NewSelect label={'Testing Select'} id={'select'} none>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-      </NewSelect>
+      <Grid container spacing={2}>
+                <Grid item lg={3} md={6} sm={6} xs={12}>
+                <SimpleInfoCard title={'Number of members'} value={2540} sx={{backgroundColor: 'secondary.light'}}/>
+                </Grid>
+                <Grid item lg={3} md={6} sm={6} xs={12}>
+                <SimpleInfoCard title={'Number of members'} value={2540} sx={{backgroundColor: 'warning.light'}}/>
+                </Grid>
+                <Grid item lg={3} md={6} sm={6} xs={12}>
+                <SimpleInfoCard title={'Number of members'} value={2540} sx={{backgroundColor: 'success.light'}}/>
+                </Grid>
+                <Grid item lg={3} md={6} sm={6} xs={12}>
+                <SimpleInfoCard title={'Number of members'} value={2540} sx={{backgroundColor: 'error.light'}}/>
+                </Grid>
+            </Grid>
     </Sidebar>
   );
 }
