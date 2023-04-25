132b42486d691fde9477bf85206b88fc3702f185
gihanchaturanga <gihanchaturanga98@gmail.com>
2023-04-25 22:46:45 +0530

    change marginTop of form element

--- src/common/Elements/FormElement/FormElement.jsx
+++ src/common/Elements/FormElement/FormElement.jsx
@@ -5,7 +5,7 @@ const FormElement = ({children, title, sub, top}) => {
   return (
     <>
         <Grid container sx={{marginTop: top}}>
-            <Grid item lg={5} md={6} sm={8} xs={12} boxShadow={5} marginX={'auto'} marginY={4} paddingX={2} paddingY={2} sx={{borderRadius: 2}}>
+            <Grid item lg={5} md={6} sm={8} xs={12} boxShadow={5} marginX={'auto'} paddingX={2} paddingY={2} sx={{borderRadius: 2}}>
                 <Typography sx={{textAlign: 'center', fontWeight: 'bold'}} color={'primary'} variant='h5'>{title}</Typography>
                 <Typography sx={{textAlign: 'center', marginBottom: 2}}  variant='subtitle1'>{sub}</Typography>
                 {children}
